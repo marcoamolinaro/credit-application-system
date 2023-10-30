f6f6788a746e915d6968d989d2a5a2448b95fcff
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-10-30 16:36:19 -0300

    Added annotations on Entity classes

--- src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,12 +1,36 @@
 package me.dio.credit.application.system.entity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "cliente")
 data class Customer (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
+    @Column(nullable = false)
     var firstName: String = "",
+
+    @Column(nullable = false)
     var lastName: String = "",
+
+    @Column(nullable = false, unique = true)
     val cpf: String,
+
+    @Column(nullable = false, unique = true)
     var email: String = "",
+
+    @Column(nullable = false)
     var password: String = "",
+
+    @Embedded
+    @Column(nullable = false)
     var address: Address = Address(),
+
+    @OneToMany(fetch = FetchType.LAZY,
+        cascade = [CascadeType.REMOVE, CascadeType.PERSIST],
+        mappedBy = "customer")
+    @Column(nullable = false)
     var credit: List<Credit> = mutableListOf()
 )
