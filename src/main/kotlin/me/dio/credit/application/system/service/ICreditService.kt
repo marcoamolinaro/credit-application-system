9e585b9f9d0da4c2b013dfad3bba48fbe2e40c08
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-10-31 17:51:32 -0300

    Added CreditService implementation

--- src/main/kotlin/me/dio/credit/application/system/service/ICreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/ICreditService.kt
@@ -1,12 +1,14 @@
 package me.dio.credit.application.system.service
 
 import me.dio.credit.application.system.entity.Credit
-import java.util.UUID
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ICreditService {
     fun save(credit: Credit): Credit
 
-    fun findAllByCustomer(customerId: Long): List<Credit>
+    @Query(value = "select * from credit where credit_id = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
 
-    fun findByCreditCode(creditCode: UUID): Credit
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit?
 }
