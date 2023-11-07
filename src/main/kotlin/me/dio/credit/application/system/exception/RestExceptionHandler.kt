cc0e15a938a6236e2ed5ab3feb73e70d5ccb078e
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-11-07 11:20:17 -0300

    Add ExceptionDetails and RestExceptionHandler

---
+++ src/main/kotlin/me/dio/credit/application/system/exception/RestExceptionHandler.kt
@@ -0,0 +1,35 @@
+package me.dio.credit.application.system.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.time.LocalDateTime
+
+@RestControllerAdvice
+class RestExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handlerValidException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionDetails> {
+        val errors: MutableMap<String, String?> = HashMap()
+        ex.bindingResult.allErrors.stream().forEach {
+            error: ObjectError ->
+            val fieldName: String = (error as FieldError).field
+            val messageError: String? = error.defaultMessage
+            errors[fieldName] = messageError
+        }
+        return ResponseEntity(
+            ExceptionDetails(
+                title = "Bad Request! Please consult the documentation",
+                timeStamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.toString(),
+                details = errors
+            ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+}
